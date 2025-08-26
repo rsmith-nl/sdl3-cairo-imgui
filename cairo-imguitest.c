c30ae0d0e9ef0562ee5a116236c012e63e8fa9b0
Roland Smith <rsmith@xs4all.nl>
2025-08-26 23:12:26 +0200

    Edit comments.

--- cairo-imguitest.c
+++ cairo-imguitest.c
@@ -5,7 +5,7 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-18 14:53:46 +0200
-// Last modified: 2025-08-26T22:21:02+0200
+// Last modified: 2025-08-26T22:26:29+0200
 
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
@@ -31,7 +31,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 {
   (void)argc;
   (void)argv;
-  // Initialize context
+  // Initialize state needed in all functions.
   static State s = {0};
   // Create GUI context.
   static GUI_context ctx = {0};
@@ -66,6 +66,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 {
   (void)appstate;
   State *s = appstate;
+  // GUI definition starts here.
   gui_begin(s->renderer, s->texture, s->ctx);
   if (gui_button(s->ctx, 10, 10, "Test")) {
       puts("buttom pressed");
@@ -91,6 +92,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
       // puts("switching to dark theme.");
     }
   }
+  // You can still draw to s->ctx here...
+  // End of GUI definition
   gui_end(s->ctx);
   return SDL_APP_CONTINUE;
 }
@@ -98,7 +101,6 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
   State *s = appstate;
-  // TODO: move to cairo-imgui.c
   return gui_process_events(s->ctx, event);
 }
 
